7b2c700ffd905661be37fb3c1a9d1da28f5d71f5
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-02-21 21:04:04 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,41 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\about_controller;
+use App\Http\Controllers\components_controller;
+use App\Http\Controllers\contacts_controller;
+use App\Http\Controllers\index_controller;
+use App\Http\Controllers\Authentication;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return view('index');
 });
+
+Route::get('/components',[components_controller::class, 'components'])->name('components');
+Route::get('/about',[about_controller::class, 'about'])->name('about');
+Route::get('/contact',[contacts_controller::class, 'contact'])->name('contact');
+Route::get('/index',[index_controller::class, 'index'])->name('index');
+
+
+Route::get('/login',[Authentication::class, 'login'])->name('login');
+Route::post('/login',[Authentication::class,'loginPost'])->name('login.post');
+Route::get('/registration',[Authentication::class,'registration'])->name('registration');
+Route::post('/registration',[Authentication::class,'registrationPost'])->name('registration.post');
+Route::get('/logout',[Authentication::class,'logout'])->name('logout');
+
+Route::get('/reset-request', [ResetPasswordController::class, 'showResetRequestForm'])->name('reset.request.form');
+Route::post('/reset-request', [ResetPasswordController::class, 'showResetPasswordPost'])->name('reset.request.post');
+Route::get('/reset-password', [ResetPasswordController::class, 'showResetPasswordForm'])->name('reset.password.form');
+Route::post('/reset-password', [ResetPasswordController::class, 'resetPasswordPost'])->name('reset.password.post');
