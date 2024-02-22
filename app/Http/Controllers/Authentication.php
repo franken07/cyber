715e6293f95b59b84bfc4810e7a389b88c862d4c
nicole <153366170+ZhyrexCalamiong@users.noreply.github.com>
2024-02-22 10:33:16 +0800

    commited

--- app/Http/Controllers/Authentication.php
+++ app/Http/Controllers/Authentication.php
@@ -60,7 +60,8 @@ class Authentication extends Controller
         $request->validate([
             	'name' => 'required',
             	'email' => 'required|email|unique:users',
-            	'password' => 'required|confirmed',
+            	'password' => 'required',
+                'password_confirmation' => 'required|same:password',
 		        'phone' => 'required',
 		        'address' => 'required'
             
@@ -70,6 +71,7 @@ class Authentication extends Controller
         $data['name'] = $request->name;
         $data['email'] = $request->email;
         $data['password'] = Hash::make($request->password);
+        $data['password_confirmation'] = $request->password_confirmation;
 	    $data['phone'] = $request->phone;
 	    $data['address'] = $request->address;
     
