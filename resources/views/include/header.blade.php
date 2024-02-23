435d6b15cfac00b3d97ebb5ffd298c8d04c7bb36
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-02-23 11:28:19 +0800

    Update header.blade.php

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -40,7 +40,7 @@
         <div class="navbar-buttons mbr-section-btn">
           <a class="btn btn-primary display-4" href="{{ route('login') }}" style="background-color: yellow; color: black;">
             <i class="fas fa-user" style="font-size: 24px; margin-right: 10px;"></i>
-              
+              LOGIN
           </a>
       </div>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
