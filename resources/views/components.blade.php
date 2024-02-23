577ebc0969bc2d4b62dab532a62411b1786954fe
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-02-23 11:16:59 +0800

    Update components.blade.php

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -31,54 +31,7 @@
 </head>
 <body>
 
- <!-- Navbar  -->
-<section class="menu menu2 cid-u3GZCsGXbm" once="menu" id="menu-5-u3GZCsGXbm">
-  <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
-    <div class="container">
-      <div class="navbar-brand">
-        <span class="navbar-logo">
-          <a href="{{ route('index') }}">
-						<img src="assets/images/loko.png" style="height: 4.3rem;">
-					</a>
-				</span>
-      </div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse"
-        data-bs-toggle="collapse" data-target="#navbarSupportedContent"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <div class="hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
-          <li class="nav-item">
-            <a class="nav-link link text-black display-4" href="#cpu">CPU</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link text-black display-4" href="#gpu">GPU</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link text-black display-4" href="about.html">MONITOR</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link text-black display-4" href="{{ route('contact') }}">Contacts</a>
-          </li>
-        </ul>
-        <div class="navbar-buttons mbr-section-btn">
-          <a class="btn btn-primary display-4" href="shop.html" style="background-color: yellow; color: black;">
-              <i class="fas fa-user" style="font-size: 24px; margin-right: 10px;"></i>
-              LOGIN
-          </a>
-      </div>
-      </div>
-    </div>
-  </nav>
-</section>
+ 
 
 
  <!--####################### CPU Starts Here ###################-->
