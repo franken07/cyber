c16b5f8897b123e4f296a6d27b6a169e5e369198
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-02-23 13:25:37 +0800

    Update header.blade.php

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -22,7 +22,7 @@
           <span></span>
         </div>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link link text-black display-4" href="{{ route('components') }}">Components</a>
