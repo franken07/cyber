e891bfee9065a7e609f0882d71ca8483e58ff03c
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-02-23 12:29:38 +0800

    Update header.blade.php

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -40,7 +40,7 @@
         <div class="navbar-buttons mbr-section-btn">
             @guest
           <a class="btn btn-primary display-4" href="{{ route('login') }}" style="background-color: yellow; color: black;">
-            <i class="fas fa-user" style="font-size: 24px; margin-right: 10px;"></i>
+            <i class="fas fa-user" style="font-size: 24px; margin-right: 50px;"></i>
               LOGIN
           </a>
           @endguest
