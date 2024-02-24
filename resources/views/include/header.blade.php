3d2a59e1e559d08e84e61b4b134ac08e55a1baac
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-02-24 21:03:19 +0800

    Update header.blade.php

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -37,7 +37,7 @@
             <a class="nav-link link text-black display-4" href="{{ route('contact') }}">Contacts</a>
           </li>
         </ul>
-        <div class="navbar-buttons mbr-section-btn d-flex justify-content-end">
+        <div class="navbar-buttons mbr-section-btn align-items-left">
             @guest    
           <a class="btn btn-primary display-4" href="{{ route('login') }}" style="background-color: yellow; color: black;">
             <i class="fas fa-user" style="font-size: 24px; margin-right: 10px;"></i>
