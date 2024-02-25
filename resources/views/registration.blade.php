d15b2678766d650a1096cd79db4c36cb0339b802
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-02-25 09:12:52 +0800

    Update registration.blade.php

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
-    <link rel="stylesheet" href="assets/registration.css">
+    <link rel="stylesheet" href="assets/registrationcss/registrationn.css">
 </head>
 <body>
 <div class="container">
