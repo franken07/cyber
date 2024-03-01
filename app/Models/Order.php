121648d6673220ef958be72fbc37d8a9bf3855a1
nicole <153366170+ZhyrexCalamiong@users.noreply.github.com>
2024-03-01 12:06:56 +0800

    model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'prod_name',
+        'image',
+        'price',
+        'product_id',
+        'user_id',
+        'quantity'
+    ];
 }
