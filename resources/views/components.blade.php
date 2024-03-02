e3b6a7b545306fda7d4f5da1a09e845fc4581891
nicole <153366170+ZhyrexCalamiong@users.noreply.github.com>
2024-03-02 11:53:15 +0800

    Update components.blade.php

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -19,7 +19,7 @@
   <link rel="stylesheet" href="assets/socicon/css/styles.css">
   <link rel="stylesheet" href="assets/animatecss/animate.css">
   <link rel="stylesheet" href="assets/theme/css/style.css">
-  <link rel="stylesheet" href="assets/addtocartpopup/popup.css">
+  
   <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@400;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
   <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@400;700&display=swap"></noscript>
   <link rel="preload" as="style" href="assets/mobirise/css/additional.css"><link rel="stylesheet" href="assets/mobirise/css/additional.css" type="text/css">
@@ -61,14 +61,7 @@
                             @csrf
                             <input type="number" name="quantity" value="1" min="1" class="form-control" required>
                             <input type="hidden" name="product_id" value="{{ $product->id }}">
-                            <button id=addToCartButton type="submit" class="btn btn-primary">Add to Cart</button>
-
-                            <div id="cartPopup" class="popup">
-                            <div class="popup-content">
-                            <span class="close">&times;</span>
-                            <p>Added to Cart!</p>
-                         </div>
-                         </div>
+                            <button type="submit" class="btn btn-primary">Add to Cart</button>
                         </form>
                     </div>
                 </div>
@@ -125,7 +118,7 @@
   <script src="assets/smoothscroll/smooth-scroll.js"></script>
   <script src="assets/ytplayer/index.js"></script>
   <script src="assets/theme/js/script.js"></script>
-  <script src="assets/jspopup/pop.js"></script>
+
   <script src="assets/formoid/formoid.min.js"></script>
   
   
