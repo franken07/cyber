99f6ba5906970bbd14fcd4c2bc9d9388c5e96a30
nicole <153366170+ZhyrexCalamiong@users.noreply.github.com>
2024-03-08 13:45:20 +0800

    Update addcart.blade.php

--- resources/views/addcart.blade.php
+++ resources/views/addcart.blade.php
@@ -69,11 +69,7 @@
             @endforeach
             </tbody> 
         </table>
-        <div>
-            <div>Total Price: $<span id="total-price">{{ $totalprice }}</span></div> <!-- Display total price -->
-            <button type="submit" id="proceed-to-order-btn">CHECK OUT SELECTED</button>
-            <label><input type="checkbox" id="check-all"> Check All</label>
-        </div>
+        @include('include.checkout')
     </form>
 @endif
 
