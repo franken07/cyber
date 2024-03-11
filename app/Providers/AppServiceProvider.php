adecd908a5e36e92b6e9654a9938b75efde697a0
Kimbert Soriano <sorianokimbertmark@gmail.com>
2024-03-11 09:55:41 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
+     * Bootstrap any application services.
      */
-    public function register()
+    public function boot()
     {
-        //
+        // Check if the application is running in a production environment
+        if ($this->app->environment('production')) {
+            // Force the application to use 'https' scheme for all generated URLs
+            URL::forceScheme('https');
+        }
     }
 
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
+     * Register any application services.
      */
-    public function boot()
+    public function register()
     {
         //
     }
