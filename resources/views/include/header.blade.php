4b93caf9fd6acd02c5cf337d65fac88d30e621e5
nicole <153366170+ZhyrexCalamiong@users.noreply.github.com>
2024-03-16 06:58:24 +0800

    Update header.blade.php

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,4 +1,3 @@
-
 <!-- Navbar  -->
 <section class="menu menu2 cid-u3GZCsGXbm" once="menu" id="menu-5-u3GZCsGXbm">
   <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
@@ -6,9 +5,9 @@
       <div class="navbar-brand">
         <span class="navbar-logo">
           <a href="{{ route('index') }}">                            
-						<img src="images/loko.png" style="height: 4.3rem;">
-					</a>
-				</span>
+            <img src="images/loko.png" style="height: 4.3rem;">
+          </a>
+        </span>
       </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse"
         data-bs-toggle="collapse" data-target="#navbarSupportedContent"
@@ -22,8 +21,8 @@
           <span></span>
         </div>
       </button>
-      <div class="collapse navbar-collapse justify-content-between  " id="navbarSupportedContent">
-         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link link text-black display-4" href="{{ route('components') }}">Components</a>
           </li>
@@ -36,21 +35,28 @@
           <li class="nav-item">
             <a class="nav-link link text-black display-4" href="{{ route('contact') }}">Contacts</a>
           </li>
+          @auth
+          @if(auth()->user()->usertype == 1) <!-- Check if user type is 1 (admin) -->
+          <li class="nav-item"> <!-- Add admin link -->
+            <a class="nav-link link text-black display-4" href="{{ route('admin.dashboard') }}">Admin</a>
+          </li>
+          @endif
+          @endauth
         </ul>
         <div class="navbar-buttons mbr-section-btn align-items-left">
-            @guest    
+          @guest    
           <a class="btn btn-primary display-4" href="{{ route('login') }}" style="background-color: yellow; color: black;">
             <i class="fas fa-user" style="font-size: 24px; margin-right: 10px;"></i>
               LOGIN
           </a>
           @endguest
-            @auth
+          @auth
           <a class="btn btn-primary display-4" href="{{ route('logout') }}" style="background-color: yellow; color: black;">  
             <i class="fas fa-user" style="font-size: 24px; margin-right: 10px;"></i>
               LOGOUT
           </a>
-          @endguest
-      </div>
+          @endauth
+        </div>
       </div>
     </div>
   </nav>
